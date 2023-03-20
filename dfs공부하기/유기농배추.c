dee5a598aa9759b317610f76e3065b8e9f1f7d77
DongwookHong <hduk4902@naver.com>
2023-03-20 12:49:12 +0900

    dfs

--- dfs공부하기/유기농배추.c
+++ dfs공부하기/유기농배추.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
 #define max 51
+
 int M,N;
 int square[max][max];
 
@@ -8,16 +8,16 @@ int dx[4] = {-1,1,0,0};
 int dy[4] = {0,0,-1,1};
 
 
-void dfs(int a,int b)
+void bfs(int a,int b)
 {
-    square[a][b]=0;
+    square[a][b]==0;
    
     for(int i=0; i<4; i++)
     {
         int x = a+ dx[i];
         int y = b+ dy[i];
         if(x>=0 && x<M && y>=0 && y< N && square[x][y]  ==1)
-            dfs(x,y);     
+            bfs(x,y);     
     }
 }
 int main()
@@ -25,7 +25,6 @@ int main()
     int test;
     scanf("%d" , &test);
     int k;
-    int cnt;
     for(int i =0; i < test; i++)
     {
         scanf("%d %d %d", &M,&N,&k );
@@ -36,20 +35,19 @@ int main()
             scanf("%d %d", &a,&b);
             square[a][b]=1;
         }
-        
-        cnt =0;
+        int cnt =0;
         for(int k =0; k < M; k++)
         {
             for(int y= 0; y <N; y++)
             {
                 if(square[k][y]==1)
                 {
-                    dfs(k,y);
+                    bfs(k,y);
                     cnt++;
                 }
             }
         }
-        printf("%d\n",cnt);
+        printf("%d",cnt);
     }
 
 }
