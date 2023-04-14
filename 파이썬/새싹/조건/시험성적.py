d7ff1b957153ab09149387fee787524989ef4861
DongwookHong <hduk4902@naver.com>
2023-04-14 17:25:38 +0900

    python

---
+++ 파이썬/새싹/조건/시험성적.py
@@ -0,0 +1,12 @@
+b =int(input())
+
+if (b>= 90):
+    print("A")
+elif (b>= 80 and b<=89):
+    print("B")
+elif (b>= 70 and b<=79):
+    print("C")
+elif (b>= 60 and b<=69):
+    print("D")
+else :
+    print("F")
